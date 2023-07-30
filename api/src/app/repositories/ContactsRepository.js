5a72cf0048b172e25dc3e549111b96c1543a067f
VitornscSilva <vitornscsilva.dev@gmail.com>
2023-07-30 13:27:24 -0300

    refactor: ContactsRepository delete method to use database instead of mocked data

--- api/src/app/repositories/ContactsRepository.js
+++ api/src/app/repositories/ContactsRepository.js
@@ -1,16 +1,5 @@
-const { v4 } = require('uuid');
-
 const db = require('../../database');
 
-let contacts = [
-  {
-    id: v4(),
-    name: 'Vitor',
-    email: 'vitornscsilva.dev@gmail.com',
-    phone: '123123',
-    categoryId: v4(),
-  },
-];
 class ContactsRepository {
   async findAll(orderBy = 'ASC') {
     const direction = orderBy.toUpperCase() === 'DESC' ? 'DESC' : 'ASC'; // prevent SQL injection
@@ -28,12 +17,9 @@ class ContactsRepository {
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-
-      resolve();
-    });
+  async delete(id) {
+    const deleteOp = await db.query('DELETE FROM contacts WHERE id = $1', [id]);
+    return deleteOp;
   }
 
   async create({
