9f2e0d50e1019c059a170823a44be70b9f1b042c
VitornscSilva <vitornscsilva.dev@gmail.com>
2023-07-30 14:12:24 -0300

    feat: add Error Handler

--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+require('express-async-errors');
 
 const routes = require('./routes');
 
@@ -7,5 +8,9 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+  console.log(error); // ToDo: add LogService
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000'));
