f73befdc65bfa13a5434b8547a54c28c70176b5c
VitornscSilva <vitornscsilva.dev@gmail.com>
2023-07-30 12:59:34 -0300

    feat: configure database and add dotenv

---
+++ api/src/database/index.js
@@ -0,0 +1,16 @@
+const { Client } = require('pg');
+
+const client = new Client({
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+});
+
+client.connect();
+
+exports.query = async (query, values) => {
+  const { rows } = await client.query(query, values);
+  return rows;
+};
