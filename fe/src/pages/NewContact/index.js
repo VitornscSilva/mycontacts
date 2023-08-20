0fe2f8b03ab6fa95c8f3fc254cecb9a0bc885556
VitornscSilva <vitornscsilva.dev@gmail.com>
2023-08-20 02:39:42 -0300

    feat: add Input, Select and Button components

--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -1,9 +1,25 @@
 import PageHeader from '../../components/PageHeader';
 
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+import Button from '../../components/Button';
+
 export default function NewContact() {
   return (
-    <PageHeader
-      title="New Contact"
-    />
+    <>
+      <PageHeader
+        title="New Contact"
+      />
+      <Input type="text" placeholder="Name" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
+      <Button>Save</Button>
+      <Button disabled>Save but disabled</Button>
+    </>
   );
 }
