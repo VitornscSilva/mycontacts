73fde65b6add2450f119552e62a994ac131fefd8
VitornscSilva <vitornscsilva.dev@gmail.com>
2023-08-28 21:03:40 -0300

    feat: add ContactForm validations

--- fe/src/components/ContactForm/index.js
+++ fe/src/components/ContactForm/index.js
@@ -1,4 +1,8 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+import isEmailValid from '../../utils/isEmailValid';
+import useErrors from '../../hooks/useErrors';
 
 import { Form, ButtonContainer } from './styles';
 
@@ -8,23 +12,77 @@ import Select from '../Select';
 import Button from '../Button';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+
+  const { setError, removeError, getErrorMessageByFieldName } = useErrors();
+
+  function handleNameChange({ target }) {
+    setName(target.value);
+
+    if (!target.value) {
+      setError({ field: 'name', message: 'Name is required' });
+    } else {
+      removeError('name');
+    }
+  }
+
+  function handleEmailChange({ target }) {
+    setEmail(target.value);
+
+    if (target.value && !isEmailValid(target.value)) {
+      setError({ field: 'email', message: 'E-mail invalid' });
+    } else {
+      removeError('email');
+    }
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log({
+      name, email, phone, category,
+    });
+  }
+
   return (
-    <Form>
-      <FormGroup>
-        <Input placeholder="Name" />
+    <Form onSubmit={handleSubmit}>
+      <FormGroup error={getErrorMessageByFieldName('name')}>
+        <Input
+          error={getErrorMessageByFieldName('name')}
+          value={name}
+          placeholder="Name"
+          onChange={handleNameChange}
+        />
+      </FormGroup>
+
+      <FormGroup error={getErrorMessageByFieldName('email')}>
+        <Input
+          error={getErrorMessageByFieldName('email')}
+          value={email}
+          placeholder="E-mail"
+          onChange={handleEmailChange}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Input placeholder="E-mail" />
+        <Input
+          value={phone}
+          placeholder="Phone"
+          onChange={({ target }) => setPhone(target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Input placeholder="Phone number" />
-      </FormGroup>
-
-      <FormGroup>
-        <Select>
+        <Select
+          value={category}
+          onChange={({ target }) => setCategory(target.value)}
+        >
+          <option value="">Category</option>
           <option value="instagram">Instagram</option>
+          <option value="discord">Discord</option>
         </Select>
       </FormGroup>
 
