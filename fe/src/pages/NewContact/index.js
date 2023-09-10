a8eab3cd0d04a368e79673de871ec06922abbcae
VitornscSilva <vitornscsilva.dev@gmail.com>
2023-09-09 23:59:18 -0300

    feat: Init integration with create contact

--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -1,14 +1,35 @@
 import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
+import ContactsService from '../../services/ContactsService';
 
 export default function NewContact() {
+  async function handleSubmit(formData) {
+    try {
+      const contact = {
+        name: formData.name,
+        email: formData.email,
+        phone: formData.phone,
+        category_id: formData.categoryId,
+      };
+
+      const response = await ContactsService.createContact(contact);
+
+      console.log({ response });
+    } catch {
+      console.log('error');
+    }
+  }
+
   return (
     <>
       <PageHeader
         title="New Contact"
       />
 
-      <ContactForm buttonLabel="Register" />
+      <ContactForm
+        buttonLabel="Register"
+        onSubmit={handleSubmit}
+      />
     </>
   );
 }
